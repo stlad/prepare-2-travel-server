1bf7f1fe484fd35bfe1eb68511c09a0e38dd4624
stlad <82213506+stlad@users.noreply.github.com>
2024-01-14 19:32:00 +0500

    upd PresetController + PresetService

--- src/main/java/ru/prepare2travel/prepare2travelserver/controller/PresetController.java
+++ src/main/java/ru/prepare2travel/prepare2travelserver/controller/PresetController.java
@@ -5,10 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.prepare2travel.prepare2travelserver.model.dto.TravelDTO;
 import ru.prepare2travel.prepare2travelserver.model.dto.TravelPresetDTO;
 import ru.prepare2travel.prepare2travelserver.service.TravelPresetService;
 import ru.prepare2travel.prepare2travelserver.service.TravelService;
@@ -23,16 +21,26 @@ import java.util.List;
 public class PresetController {
 
     @Autowired
-    private TravelService travelService;
-
-    @Autowired
     private TravelPresetService presetService;
 
     @GetMapping("/all")
     public ResponseEntity<List<TravelPresetDTO>> getAllPresets(){
-        log.info("GET to /travels/all");
+        log.info("GET to /presets/all");
 
         return new ResponseEntity<>(presetService.findAll(), HttpStatus.OK);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<TravelPresetDTO> get(@PathVariable(value = "id") Long id) {
+        log.info("GET to /presets/{id}");
+        TravelPresetDTO dto = presetService.findById(id);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<TravelPresetDTO> saveTravel(@RequestBody TravelPresetDTO presetDTO){
+        log.info("POST to /presets/");
+        presetDTO = presetService.saveTravelPreset(presetDTO);
+        return new ResponseEntity<>(presetDTO, HttpStatus.OK);
+    }
 }
--- src/main/java/ru/prepare2travel/prepare2travelserver/service/TravelPresetService.java
+++ src/main/java/ru/prepare2travel/prepare2travelserver/service/TravelPresetService.java
@@ -1,5 +1,6 @@
 package ru.prepare2travel.prepare2travelserver.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.prepare2travel.prepare2travelserver.mapper.TravelPresetMapper;
@@ -22,4 +23,15 @@ public class TravelPresetService {
         return travelPresetMapper.toDto(travelPresetRepository.findAll());
     }
 
+    public TravelPresetDTO findById(Long id){
+        TravelPreset preset = travelPresetRepository.findById(id).orElseThrow(()->
+                new EntityNotFoundException("Cannot find entity with id: "+id));
+        return travelPresetMapper.toDto(preset);
+    }
+
+    public TravelPresetDTO saveTravelPreset(TravelPresetDTO dto){
+        TravelPreset preset = travelPresetMapper.fromDto(dto);
+        preset = travelPresetRepository.save(preset);
+        return travelPresetMapper.toDto(preset);
+    }
 }
